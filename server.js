358e26f5b2c52afae81e855063d7d509ec1ee2de
paul449 <paul_bilbatua@hotmail.com>
2024-05-31 16:31:28 -0500

    notes.html

--- server.js
+++ server.js
@@ -8,11 +8,16 @@ const path = require('path');
 const fs = require('fs');
 //assigning port number
 const PORT = process.env.PORT || 3003;
+//
+
 //setting middleware to parse request
 app.use(express.static(path.join(__dirname,"public")));
 //test middleware
 app.get('/',(req,res)=>{
     res.sendFile('index.html')
+});
+app.get('/notes',(req,res)=>{
+    res.sendFile(path.join(__dirname,'public/notes.html'))
 })
 //running application through port 3001
 app.listen(PORT,()=>{
