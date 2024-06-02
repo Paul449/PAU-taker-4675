73630a8a615b3a44ca2c8c99098c71f771012af4
paul449 <paul_bilbatua@hotmail.com>
2024-06-02 17:20:05 -0500

    readfile

--- routes/notes-route.js
+++ routes/notes-route.js
@@ -4,23 +4,27 @@ const Router = require('express').Router();
 //importing file system
 const fs = require('fs');
 //importing path library
-const path = 'db.json';
+const path = require('path');
+//import db
+const db = require('../db/db.json');
+//string JSON
 //reading db.json
-function ReadFile(){
-
-}
-
-Router.get('api/notes',async(req,res)=>{
+async function ReadFile(){
     try{
-
-    }catch{
-
+        const Data = fs.readFileSync(db,"utf-8");
+        console.log('dataDB',Data)
+    }catch(error){
+        console.error("An error has ocurred:",error)
     }
+}
+ReadFile()
+Router.get('api/notes',async(req,res)=>{
+   
 });
 
 //creating note
 function writeFile(){
-    
+
 }
 
 //POST /api/notes
