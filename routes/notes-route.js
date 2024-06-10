05d38c9e23ab70b6fb6e1a544ea8ff517ca4526a
paul449 <paul_bilbatua@hotmail.com>
2024-06-09 19:00:27 -0500

    files

--- routes/notes-route.js
+++ routes/notes-route.js
@@ -1,40 +1,42 @@
 //get api/notes should read the db.json
 //importing express router
 const Router = require('express').Router();
-//importing file system
-const fs = require('fs');
+//importing file system, allowing promises to be applied
+const fs = require('fs').promises;
 //importing path library
 const path = require('path');
-//import db
-const db = require('../db/db.json');
 //string JSON
 //reading db.json
 async function ReadFile(){
     try{
-        const Data = fs.readFile(path.join(__dirname,db.notes),"utf-8");
+        const Data = fs.readFile(path.join(__dirname,'../db/db.json'),"utf-8");
         console.log('dataDB',Data)
+        return JSON.parse(Data); 
     }catch(error){
-        console.error("An error has ocurred:",error)
+        console.error("An error has ocurred:",error);
     }
 }
-readFile();
 //route for reading created note
 Router.get('/notes',async(req,res)=>{
-  res.send(ReadFile);
+  let Notes = await ReadFile();
+  res.send(Notes);
 });
 
 //creating note
-async function writeFile(){
+async function writeFile(data){
     try{
-        let Note = fs.writeFile(path.join(__dirname,db))
+        await fs.writeFile(path.join(__dirname,'../db/db.json'),JSON.stringify(data))
     }catch(error){
-
+        console.error('An error has ocurred:',error);
     }
 }
-writeFile();
 //POST /api/notes
 Router.post('/notes',async(req,res)=>{
-
+let NoteBody = req.body;
+let Notes = await writeFile();
+Notes.push(NoteBody);
+await writeFile(Notes);
+res.json(NoteBody);
 })
 
 module.exports = Router;
